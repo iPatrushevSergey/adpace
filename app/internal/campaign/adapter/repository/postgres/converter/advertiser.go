ac6dadfd861c89e014569b407b36ec17216e2144
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-21 14:59:58 +0300

    feat(campaign|adapter): add advertiser converter interface
    Added AdvertiserConverter goverter interface: ToEntityAdvertiser, ToCreateParams, ToPutParams.

---
+++ app/internal/campaign/adapter/repository/postgres/converter/advertiser.go
@@ -0,0 +1,20 @@
+package converter
+
+//go:generate go run github.com/jmattheis/goverter/cmd/goverter@v1.11.0 gen .
+
+import (
+	"github.com/iPatrushevSergey/adpace/app/internal/campaign/adapter/repository/postgres/sqlcgen"
+	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain/entity"
+)
+
+// goverter:converter
+// goverter:output:file advertiser_generated.go
+// goverter:extend UUIDToString
+// goverter:extend StringToUUID
+// goverter:extend CopyTime
+// goverter:extend CopyTimePtr
+type AdvertiserConverter interface {
+	ToEntityAdvertiser(source sqlcgen.Advertiser) entity.Advertiser
+	ToCreateParams(source entity.Advertiser) (sqlcgen.CreateParams, error)
+	ToPutParams(source entity.Advertiser) (sqlcgen.PutParams, error)
+}
