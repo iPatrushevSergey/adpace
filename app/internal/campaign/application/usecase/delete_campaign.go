5011034d7f5359070b6893d0de0d754bc1568964
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-23 23:28:17 +0300

    feat(campaign|application): add DeleteCampaign use case
    Plain passthrough to CampaignRepo.Delete.

---
+++ app/internal/campaign/application/usecase/delete_campaign.go
@@ -0,0 +1,28 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/dto"
+	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/port"
+	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain"
+	"github.com/iPatrushevSergey/adpace/app/internal/pkg/apputil"
+)
+
+type DeleteCampaign struct {
+	campaignRepo port.CampaignRepo
+}
+
+func NewDeleteCampaign(campaignRepo port.CampaignRepo) *DeleteCampaign {
+	return &DeleteCampaign{campaignRepo: campaignRepo}
+}
+
+func (uc *DeleteCampaign) Execute(ctx context.Context, in dto.DeleteCampaignInput) (struct{}, error) {
+	if !apputil.IsUUID(in.CampaignID) {
+		return struct{}{}, domain.ErrBadInput
+	}
+	if err := uc.campaignRepo.Delete(ctx, in.CampaignID); err != nil {
+		return struct{}{}, err
+	}
+	return struct{}{}, nil
+}
