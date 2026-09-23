33608b6effd47d51e45a3335e64e6838e7ec3e30
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-23 23:23:40 +0300

    feat(campaign|application): add CampaignUseCases
    Groups Create/Get/Patch/Put/Delete/Pause/Resume use cases exposed to the presentation layer.

--- app/internal/campaign/application/usecase/usecases.go
+++ app/internal/campaign/application/usecase/usecases.go
@@ -14,3 +14,13 @@ type AdvertiserUseCases struct {
 	Put    port.UseCase[dto.PutAdvertiserInput, struct{}]
 	Delete port.UseCase[dto.DeleteAdvertiserInput, struct{}]
 }
+
+type CampaignUseCases struct {
+	Create port.UseCase[dto.CreateCampaignInput, entity.Campaign]
+	Get    port.UseCase[dto.GetCampaignInput, entity.Campaign]
+	Patch  port.UseCase[dto.PatchCampaignInput, entity.Campaign]
+	Put    port.UseCase[dto.PutCampaignInput, struct{}]
+	Delete port.UseCase[dto.DeleteCampaignInput, struct{}]
+	Pause  port.UseCase[dto.PauseCampaignInput, entity.Campaign]
+	Resume port.UseCase[dto.ResumeCampaignInput, entity.Campaign]
+}
