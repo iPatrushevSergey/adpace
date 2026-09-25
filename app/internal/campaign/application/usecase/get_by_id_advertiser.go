3cb9e3e2bfe1183dc7a674d277e4f8a7f6905032
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 00:28:09 +0300

    refactor(campaign|application): drop UUID check from GetByIDAdvertiser
    Format is now validated once at presentation via uuid.Parse.

--- app/internal/campaign/application/usecase/get_by_id_advertiser.go
+++ app/internal/campaign/application/usecase/get_by_id_advertiser.go
@@ -5,9 +5,7 @@ import (
 
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/dto"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/port"
-	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain/entity"
-	"github.com/iPatrushevSergey/adpace/app/internal/pkg/apputil"
 )
 
 type GetByIDAdvertiser struct {
@@ -23,10 +21,6 @@ func NewGetByIDAdvertiser(
 }
 
 func (uc *GetByIDAdvertiser) Execute(ctx context.Context, in dto.GetAdvertiserInput) (out entity.Advertiser, err error) {
-	if !apputil.IsUUID(in.AdvertiserID) {
-		return entity.Advertiser{}, domain.ErrBadInput
-	}
-
 	advertiser, err := uc.advertiserRepo.GetByID(ctx, in.AdvertiserID)
 	if err != nil {
 		return entity.Advertiser{}, err
