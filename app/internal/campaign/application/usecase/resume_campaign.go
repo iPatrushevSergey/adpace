85934b21178b3758d4f926fd9b456282d85769fb
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 00:28:57 +0300

    refactor(campaign|application): drop UUID check from ResumeCampaign

--- app/internal/campaign/application/usecase/resume_campaign.go
+++ app/internal/campaign/application/usecase/resume_campaign.go
@@ -2,13 +2,10 @@ package usecase
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/dto"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/port"
-	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain/entity"
-	"github.com/iPatrushevSergey/adpace/app/internal/pkg/apputil"
 )
 
 type ResumeCampaign struct {
@@ -27,9 +24,5 @@ func NewResumeCampaign(
 }
 
 func (uc *ResumeCampaign) Execute(ctx context.Context, in dto.ResumeCampaignInput) (out entity.Campaign, err error) {
-	if !apputil.IsUUID(in.CampaignID) {
-		return entity.Campaign{}, fmt.Errorf("%w: campaign id is not a valid UUID", domain.ErrBadInput)
-	}
-
 	return uc.campaignRepo.Resume(ctx, in.CampaignID, uc.clock.Now())
 }
