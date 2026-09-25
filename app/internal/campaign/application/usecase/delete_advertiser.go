306e0f5352054e90b15ac9dd36b209a39cd4d65f
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 00:28:24 +0300

    refactor(campaign|application): drop UUID check from DeleteAdvertiser

--- app/internal/campaign/application/usecase/delete_advertiser.go
+++ app/internal/campaign/application/usecase/delete_advertiser.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/dto"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/application/port"
-	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain"
-	"github.com/iPatrushevSergey/adpace/app/internal/pkg/apputil"
 )
 
 type DeleteAdvertiser struct {
@@ -18,10 +16,6 @@ func NewDeleteAdvertiser(advertiserRepo port.AdvertiserRepo) *DeleteAdvertiser {
 }
 
 func (uc *DeleteAdvertiser) Execute(ctx context.Context, in dto.DeleteAdvertiserInput) (out struct{}, err error) {
-	if !apputil.IsUUID(in.AdvertiserID) {
-		return struct{}{}, domain.ErrBadInput
-	}
-
 	if err := uc.advertiserRepo.Delete(ctx, in.AdvertiserID); err != nil {
 		return struct{}{}, err
 	}
