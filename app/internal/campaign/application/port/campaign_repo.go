9c01bfa70914af058e10be95f9ad50566a18e541
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 23:45:40 +0300

    refactor(campaign|application): use uuid.UUID in CampaignRepo

--- app/internal/campaign/application/port/campaign_repo.go
+++ app/internal/campaign/application/port/campaign_repo.go
@@ -4,15 +4,16 @@ import (
 	"context"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain/entity"
 )
 
 type CampaignRepo interface {
-	GetByID(ctx context.Context, campaignID string) (entity.Campaign, error)
-	GetByIDForUpdate(ctx context.Context, campaignID string) (entity.Campaign, error)
+	GetByID(ctx context.Context, campaignID uuid.UUID) (entity.Campaign, error)
+	GetByIDForUpdate(ctx context.Context, campaignID uuid.UUID) (entity.Campaign, error)
 	Create(ctx context.Context, campaign entity.Campaign) (entity.Campaign, error)
 	Save(ctx context.Context, campaign entity.Campaign) error
-	Pause(ctx context.Context, campaignID, reason string, updatedAt time.Time) (entity.Campaign, error)
-	Resume(ctx context.Context, campaignID string, updatedAt time.Time) (entity.Campaign, error)
-	Delete(ctx context.Context, campaignID string) error
+	Pause(ctx context.Context, campaignID uuid.UUID, reason string, updatedAt time.Time) (entity.Campaign, error)
+	Resume(ctx context.Context, campaignID uuid.UUID, updatedAt time.Time) (entity.Campaign, error)
+	Delete(ctx context.Context, campaignID uuid.UUID) error
 }
