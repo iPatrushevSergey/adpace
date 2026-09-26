15fb3b190eec15dbac54fc6a41eeaf0d4cffb57f
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 23:46:02 +0300

    refactor(campaign|adapter): IDGenerator returns uuid.UUID

--- app/internal/campaign/adapter/generator/id_generator.go
+++ app/internal/campaign/adapter/generator/id_generator.go
@@ -8,10 +8,6 @@ func NewIDGenerator() *IDGenerator {
 	return &IDGenerator{}
 }
 
-func (g *IDGenerator) NewID() (string, error) {
-	id, err := uuid.NewV7()
-	if err != nil {
-		return "", err
-	}
-	return id.String(), nil
+func (g *IDGenerator) NewID() (uuid.UUID, error) {
+	return uuid.NewV7()
 }
