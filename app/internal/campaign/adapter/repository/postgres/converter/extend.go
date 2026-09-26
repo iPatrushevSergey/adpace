8ca989017b4ffa30d92c9924b606b7a9880bf9cf
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 23:46:08 +0300

    refactor(campaign|adapter): drop UUID string extends

--- app/internal/campaign/adapter/repository/postgres/converter/extend.go
+++ app/internal/campaign/adapter/repository/postgres/converter/extend.go
@@ -1,20 +1,6 @@
 package converter
 
-import (
-	"time"
-
-	"github.com/google/uuid"
-)
-
-// UUIDToString converts a database UUID to a domain id.
-func UUIDToString(id uuid.UUID) string {
-	return id.String()
-}
-
-// StringToUUID converts a domain id to a database UUID.
-func StringToUUID(id string) (uuid.UUID, error) {
-	return uuid.Parse(id)
-}
+import "time"
 
 // CopyTime maps time.Time for goverter (required: time.Time has unexported fields).
 func CopyTime(v time.Time) time.Time {
