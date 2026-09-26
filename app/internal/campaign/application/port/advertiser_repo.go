a1ad3043d6f7cae196a75201c43b97a481eee3e2
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-09-26 23:45:37 +0300

    refactor(campaign|application): use uuid.UUID in AdvertiserRepo

--- app/internal/campaign/application/port/advertiser_repo.go
+++ app/internal/campaign/application/port/advertiser_repo.go
@@ -3,13 +3,14 @@ package port
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/iPatrushevSergey/adpace/app/internal/campaign/domain/entity"
 )
 
 type AdvertiserRepo interface {
-	GetByID(ctx context.Context, advertiserID string) (entity.Advertiser, error)
-	GetByIDForUpdate(ctx context.Context, advertiserID string) (entity.Advertiser, error)
+	GetByID(ctx context.Context, advertiserID uuid.UUID) (entity.Advertiser, error)
+	GetByIDForUpdate(ctx context.Context, advertiserID uuid.UUID) (entity.Advertiser, error)
 	Create(ctx context.Context, advertiser entity.Advertiser) (entity.Advertiser, error)
 	Save(ctx context.Context, advertiser entity.Advertiser) error
-	Delete(ctx context.Context, advertiserID string) error
+	Delete(ctx context.Context, advertiserID uuid.UUID) error
 }
